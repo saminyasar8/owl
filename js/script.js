376d0e722d8a1380e3cd35d7b2ccf0b800b719ca
saminyasar8 <75202097+saminyasar8@users.noreply.github.com>
2020-12-10 13:48:51 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,62 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay: true,
+    autoplayTimeout: 2500,
+    dots:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        },
+    }
+})
+
+<br>
+
+$('.owl-carousel 1').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay: true,
+    autoplayTimeout: 2500,
+    dots:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        },
+    }
+})
+
+<br>
+$('.owl-carousel 2').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    dots:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        },
+    }
+})
